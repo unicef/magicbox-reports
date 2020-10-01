060d49e25611d74f2068b2236aa9e9938c4aac71
merlos <merlos@users.noreply.github.com>
2020-10-01 16:05:47 -0400

    customize loading

--- lib/UnicefDash.py
+++ lib/UnicefDash.py
@@ -3,9 +3,21 @@ import dash_html_components as html
 
 
 class UnicefDash(dash.Dash):
+    
+    
     def interpolate_index(self, **kwargs):
         # Inspect the arguments by printing them
         #print(kwargs)
+        kwargs['app_entry'] = """
+    <div id="react-entry-point">
+                    <div class="_dash_loading d-flex justify-content-center align-items-center">
+                        <div class="d-flex justify-content-center align-items-center">
+                          <div class="spinner-border" role="status" aria-hidden="true"></div>
+                            <strong class='ml-2'>Loading...</strong>
+                          </div>
+                    </div>
+                </div>"""
+
         return '''
         <!DOCTYPE html>
         <html>
