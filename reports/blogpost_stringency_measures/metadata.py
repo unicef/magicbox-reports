0d611f230c791196eae9ff41913833c9c0f4d187
andre-martini <martiniandrea7@yahoo.it>
2021-02-01 20:48:53 +0100

    Changed title and few names to Blogpost

--- reports/blogpost_stringency_measures/metadata.py
+++ reports/blogpost_stringency_measures/metadata.py
@@ -1,5 +1,5 @@
 # Report title
-title = "Report 3 title "
+title = "When to implement stringent containment policies?"
 # Meta keywords
 keywords = "keyword1, keyword2, keyword3"
 # Meta description
@@ -8,6 +8,6 @@ description = "description description 3"
 summary = "Summary Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mollis pulvinar tellus vel vulputate. Integer ac massa efficitur, sagittis turpis ullamcorper, vehicula velit. Donec placerat cursus leo eu ultrices. In porttitor varius nisl. In iaculis arcu non massa ullamcorper, ac blandit dolor pulvinar. Etiam tempus urna id sapien vulputate, a congue nunc condimentum. Proin pellen "
 
 # thumbnail displayed on the homepage
-thumb = 'report3.png'
+thumb = ''
 # Alt
 thumb_alt = 'description for accessibility 3'
