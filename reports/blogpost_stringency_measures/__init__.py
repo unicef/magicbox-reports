3ce436334856560f01666efb4a32fd8e57889d5b
andre-martini <martiniandrea7@yahoo.it>
2021-02-01 20:48:05 +0100

    Added new scatterplot (to complete)

--- reports/blogpost_stringency_measures/__init__.py
+++ reports/blogpost_stringency_measures/__init__.py
@@ -7,43 +7,49 @@ from app import app
 # additional libraries
 import pandas as pd
 import plotly.graph_objs as go
+import geopandas as gpd
+import os
+
+
 
 # load data as a dataframe
-df = pd.read_csv('https://raw.githubusercontent.com/unicef/magicbox-reports/master/data/country_merged.csv')
+gdf = gpd.read_file('https://raw.githubusercontent.com/unicef/magicbox-reports/report/blogpost_stringency_measures/data/geodata_COVID-19_stringency_delay.geojson', driver='GeoJSON')
 
+# print(gdf.dtypes)
 
 def layout():
   layout = html.Div([
-    html.H3('Report 3'),
+    html.H3('Blogpost'),
     dcc.Dropdown(
-        id='app-3-dropdown',
+        id='app-blogpost-dropdown',
         options=[
-            {'label': 'Report 3 - {}'.format(i), 'value': i} for i in [
+            {'label': 'Blogpost - {}'.format(i), 'value': i} for i in [
                 'NYC', 'MTL', 'LA'
             ]
         ]
     ),
     # add scatter plot
     dcc.Graph(
-        id='travel-distance',
+        id='total_cases_at_max_stringency_day-vs-delay_high_stringency_max_cases',
         figure={
             'data': [
                 go.Scatter(
-                    x=df[df['admin2'] == i]['distance_travelled'],
-                    y=df[df['admin2'] == i]['date'],
-                    #text=df[df['admin2'] == i]['urban'],
+                    x=gdf.loc[gdf['continent'] == continent]['delay_day_max_new_cases_per_100000_first_day_sim_max_stringency_numerical_2020-03-01_2020-06-30'],
+                    y=gdf.loc[gdf['continent'] == continent]['total_cases_per_100k_at_first_day_max_stringency'],
+                    text=gdf.loc[gdf['continent'] == continent]['name'],
                     mode='markers',
                     opacity=0.8,
                     marker={
-                        'size': 13,
-                        'line': {'width': 0.5, 'color': 'white'}
+                        'size': 12, # gdf.loc[gdf['continent'] == continent]['max_stringency']
+                        'line': {'width': 0.5, 'color': 'white'},
+                        'color': i
                     },
-                    name=i
-                ) for i in df.admin2.unique()
+                    name=continent
+                ) for i,continent in enumerate(gdf['continent'].unique())
             ],
             'layout': go.Layout(
-                xaxis={'type': 'log', 'title': 'Travel Distance (km)'},
-                yaxis={'title': 'Date'},
+                xaxis={'title': 'Days difference between maximum cases and first day high stringency'},
+                yaxis={'type': 'log', 'title': 'Total COVID cases per 100k pop. at first day max stringency'},
                 margin={'l': 100, 'b': 50, 't': 50, 'r': 100},
                 legend={'x': 1, 'y': 0.5},
                 hovermode='closest'
@@ -54,7 +60,7 @@ def layout():
   return layout
 
 @app.callback(
-    Output('app-3-display-value', 'children'),
-    [Input('app-3-dropdown', 'value')])
+    Output('app-blogpost-display-value', 'blogpost'),
+    [Input('app-blogpost-dropdown', 'value')])
 def display_value(value):
     return 'You have selected "{}"'.format(value)
